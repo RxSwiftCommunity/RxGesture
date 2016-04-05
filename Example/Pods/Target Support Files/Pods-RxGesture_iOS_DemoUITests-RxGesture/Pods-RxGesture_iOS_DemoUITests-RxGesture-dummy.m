9373cd9ef2012f1572f7a8d5a0092d4c76ac9b81
Marin Todorov <marin@underplot.com>
2016-04-05 16:02:03 +0200

    * added ui test - can't run the tests :(

---
+++ Example/Pods/Target Support Files/Pods-RxGesture_iOS_DemoUITests-RxGesture/Pods-RxGesture_iOS_DemoUITests-RxGesture-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RxGesture_iOS_DemoUITests_RxGesture : NSObject
+@end
+@implementation PodsDummy_Pods_RxGesture_iOS_DemoUITests_RxGesture
+@end
---
+++ Example/Pods/Target Support Files/Pods-RxGesture_iOS_DemoUITests/Pods-RxGesture_iOS_DemoUITests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_RxGesture_iOS_DemoUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_RxGesture_iOS_DemoUITestsVersionString[];
+
