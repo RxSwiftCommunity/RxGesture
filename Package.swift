607ad588c7c95a9852a96df197b780cdbc05186d
Cihat Gündüz <github@cihatguenduez.de>
2019-03-18 16:17:26 +0100

    Add RxSwift dependency

--- Package.swift
+++ Package.swift
@@ -6,9 +6,13 @@ let package = Package(
     products: [
         .library(name: "RxGesture", targets: ["RxGesture"])
     ],
+    dependencies: [
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "4.4.0")),
+    ],
     targets: [
         .target(
             name: "RxGesture",
+            dependencies: ["RxSwift", "RxCocoa"],
             path: "Pod",
             exclude: ["Pod/Classes/OSX"]
         )
