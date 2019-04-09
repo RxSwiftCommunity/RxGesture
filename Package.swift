1fde7f32c94d03bd3a65e5127c1e461cefb2f971
pclemente <pabloclemente08@gmail.com>
2019-04-09 15:00:12 +0200

    Update Package.swift
    Update dependencies to RXSwift 4.5.0

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         .library(name: "RxGesture", targets: ["RxGesture"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "4.4.0")),
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "4.5.0")),
     ],
     targets: [
         .target(
